13888fb9cca8720144e2f47bae7b86cf57a212e3
RICKCOYL <rickoburu@gmail.com>
2020-11-23 12:47:09 +0300

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   private
+
   def require_login
     return if user_signed_in?
-      flash[:error] = 'You must be logged in to access this section'
-      redirect_to new_post_path # halts request cycle
 
-    end
+    flash[:error] = 'You must be logged in to access this section'
+    redirect_to new_post_path # halts request cycle
+  end
 end
